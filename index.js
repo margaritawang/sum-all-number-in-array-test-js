818948f97bff8e657655753f9efa7eed7e603157
Margarita Amy Wang <amywang.2@alumni.ubc.ca>
2018-01-03 00:56:59 +0000

    added recursive function

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+
+  for (i of array) {
+    if (Array.isArray(i)) {
+      sum += sumItems(i);
+    } else {
+      console.log('number:', i);
+      sum += i;
+      console.log('sum:', sum);
+    }
+  }
+
+  return sum;
 }
 
 module.exports = sumItems;
